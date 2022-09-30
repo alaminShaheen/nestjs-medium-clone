3e5dcdfa3847e2517be9f28c18e74699f4bd7ab0
Alamin <alaminshaheen@iut-dhaka.edu>
2022-10-01 00:47:24 +0600

    Added Host name of app

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap () {
     const document = SwaggerModule.createDocument(app, documentBuilderConfig, documentOptions);
     SwaggerModule.setup("api", app, document);
     
-    await app.listen(3000);
+    await app.listen(3000, "0.0.0.0");
     Logger.log(`Application is running on ${await app.getUrl()}`);
 }
 
